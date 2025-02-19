4d17d9c443406bfc9a42d1d0885b22655f64fd1b
rafaelwdornelas <rafaelwdornelasstl@gmail.com>
2025-02-19 00:43:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
-sudo apt-get update && sudo apt-get install rpm
+sudo apt-get update && sudo apt-get install rpm unzip
 
-wget -O PowerMTA-4.5r11.rpm https://raw.githubusercontent.com/rafaelwdornelas/autopmta/refs/heads/main/PowerMTA-4.5r11.rpm
-rpm -Uvh PowerMTA-4.5r11.rpm --nodeps --force
-wget -O pmta https://raw.githubusercontent.com/rafaelwdornelas/autopmta/refs/heads/main/pmta
-wget -O pmtad https://raw.githubusercontent.com/rafaelwdornelas/autopmta/refs/heads/main/pmtad
-wget -O pmtahttpd https://raw.githubusercontent.com/rafaelwdornelas/autopmta/refs/heads/main/pmtahttpd
-wget -O pmtasnmpd https://raw.githubusercontent.com/rafaelwdornelas/autopmta/refs/heads/main/pmtasnmpd
-wget -O license https://raw.githubusercontent.com/rafaelwdornelas/autopmta/refs/heads/main/license
+wget -O PowerMTA-5.0r7.zip https://www.comerciojobsinformativos.issmarterthanyou.com/PowerMTA-5.0r7.zip
+unzip -q PowerMTA-5.0r7.zip
+rm -rf PowerMTA-5.0r7.zip
+rpm -ivh PowerMTA-5.0r7.rpm
+mv patch/etc/pmta/license /etc/pmta
+mv patch/usr/sbin/* /usr/sbin
+chmod -R 755 /usr/sbin/pmtad
+chmod -R 755 /usr/sbin/pmtahttpd
+rm -rf PowerMTA-5.0r7.rpm
+rm -rf patch
+
 wget -O config https://raw.githubusercontent.com/rafaelwdornelas/autopmta/refs/heads/main/config
-wget -O mykey.6068805.com.pem https://raw.githubusercontent.com/rafaelwdornelas/autopmta/refs/heads/main/mykey.6068805.com.pem
+
 
 echo -n "your pmta ip:"
 read pmtaip
@@ -18,15 +22,8 @@ echo -n "your pmta port:"
 read pmtaport
 
 service pmta stop
+mv config /etc/pmta
 
-rm -f /usr/sbin/pmtahttpd
-\cp license /etc/pmta/
-\cp config /etc/pmta/
-\cp mykey.6068805.com.pem /etc/pmta/mykey.$pmtahostname.pem
-\cp pmta /usr/sbin/pmta
-\cp pmtad /usr/sbin/pmtad
-\cp pmtahttpd /usr/sbin/pmtahttpd
-\cp pmtasnmpd /usr/sbin/pmtasnmpd
 
 sed -i "s/QQQipQQQ/$pmtaip/g" `grep "QQQipQQQ" -rl /etc/pmta/`
 sed -i "s/QQQhostnameQQQ/$pmtahostname/g" `grep "QQQhostnameQQQ" -rl /etc/pmta/`
